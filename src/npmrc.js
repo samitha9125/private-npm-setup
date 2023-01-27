105fce030491403b75a3e33a0553421a693acb63
Samitha <samitha@Samithas-MacBook-Pro---Calcey.local>
2023-01-27 17:37:49 +0530

    Only user level npm file will be changed

--- src/npmrc.js
+++ src/npmrc.js
@@ -4,52 +4,50 @@ const COLORS = require("./constants/colors");
 const { NPM_RC_CONTENT, NPM_USR_RC_CONTENT } = require("./constants/messages");
 const { getOSBasedNPMRCPath } = require("./helpers/index.js");
 
-function manageNPMRC(token, pvtUrl, scope) {
-  // Project level npmrc file.
-  if (fs.existsSync(".npmrc")) {
-    fs.truncateSync(".npmrc", 0);
-  }
-  const domainLess = pvtUrl.replace(/^https?:\/\//, "");
-  let content = NPM_RC_CONTENT.replace("{{TOKEN}}", token);
-  content = content.replace("{{URL}}", pvtUrl);
-  content = content.replace("{{DOMAIN}}", domainLess);
-  content = content.replace("{{SCOPE}}", scope.toLowerCase());
-  fs.writeFileSync(".npmrc", content, "utf8");
-
-  // User level npmrc file.
-  manageUserLevelNPMRC(token, domainLess);
+function manageNPMRC(token, url, scope) {
+  const domain = url.replace(/^https?:\/\//, "");
+  manageUserLevelNPMRC(token, scope, url, domain);
 }
 
-function manageUserLevelNPMRC(token, domainLess) {
+function manageUserLevelNPMRC(token, scope, url, domain) {
   const npmrcPath = getOSBasedNPMRCPath();
   if (!fs.existsSync(npmrcPath)) {
-    // If the file does not exist, create a new npmrc file.
     fs.writeFileSync(npmrcPath, "", "utf8");
   }
 
   try {
-    let newData;
-    let content = NPM_USR_RC_CONTENT.replace("{{DOMAIN}}", domainLess);
+    cleanNPMRC(npmrcPath, scope, domain);
+    let content = NPM_USR_RC_CONTENT.replace("{{DOMAIN}}", domain);
+    content = content.replace("{{SCOPE}}", scope);
+    content = content.replace("{{URL}}", url);
     content = content.replace("{{TOKEN}}", token);
 
-    const data = fs.readFileSync(npmrcPath, "utf8");
-    const regex = new RegExp(`(//${domainLess}).*`, "g");
-    if (data.match(regex)) {
-      newData = data.replace(regex, content);
-    } else {
-      newData = data.concat(`\n${content}`);
-    }
-    newData = newData
+    let data = fs.readFileSync(npmrcPath, "utf8");
+    data = data.concat(`\n${content}`);
+    data = data
       .split("\n")
       .filter((l) => l !== "")
       .join("\n");
-    fs.writeFileSync(npmrcPath, newData, "utf8");
+    fs.writeFileSync(npmrcPath, data, "utf8");
   } catch (err) {
     console.log(
       COLORS.Red,
       `Error modifying ${npmrcPath}. Error: ${err.message}`
     );
     process.exit(1);
+  }
+}
+
+function cleanNPMRC(path, scope, domain) {
+  const data = fs.readFileSync(path, "utf8");
+
+  // define the regex patterns
+  const regexPat1 = new RegExp(`@${scope}:registry=`, "g");
+  const regexPat2 = new RegExp(`${domain}/:_authToken=`, "g");
+
+  if (regexPat1.test(data) && regexPat2.test(data)) {
+    const updatedData = data.replace(pattern1, "").replace(pattern2, "");
+    fs.writeFileSync(filePath, updatedData);
   }
 }
 
